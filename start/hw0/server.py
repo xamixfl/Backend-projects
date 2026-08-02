1c4b625974f8b0d20fb80ce88dd077953c8eb933
paa24u693 <paa24u693@student.bmstu.ru>
2026-08-02 22:49:53 +0300

    resolved threads: corrected solution

--- start/hw0/server.py
+++ start/hw0/server.py
@@ -1,14 +1,19 @@
 import socket
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = 'localhost'
-port = 8080
+HOST = 'localhost'
+PORT = 8080
 
-server.bind((host, port))
-server.listen(5)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((HOST, PORT))
+    server.listen(5)
+    print("Сервер запущен и ждет подключений...")
 
-while True:
-    client, address = server.accept()
-    client.send(b"OK\n")
-    client.close()
-    
+    try:
+        while True:
+            client, address = server.accept()
+            with client:
+                client.sendall(b"OK\n")
+    except KeyboardInterrupt:
+        print("\nСервер остановлен пользователем.")
+        
