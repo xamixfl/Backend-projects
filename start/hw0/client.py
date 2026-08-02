0ac86f082505d073332778ee55a225667cfb7dfe
paa24u693 <paa24u693@student.bmstu.ru>
2026-08-02 22:43:14 +0300

    resolved threads, corrected solution

--- start/hw0/client.py
+++ start/hw0/client.py
@@ -4,12 +4,17 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = 'localhost'
 port = 8080
 
-client.connect((host, port))
-data = client.recv(1024)
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.connect((host, port))
+        data = client.recv(1024)
 
-if data == b"OK\n":
-    print("Получены корректные данные от сервера : ОК")
-else:
-    print("Данные, полученные от сервера некорректны!")
+        if data == b"OK\n":
+            print("Получены корректные данные от сервера : ОК")
+        else:
+            print("Данные, полученные от сервера некорректны!")
 
-client.close()
+except ConnectionRefusedError:
+    print(f"Не удалось подключиться к серверу {host}:{port}. Убедитесь, что сервер запущен!")
+except Exception as e:
+    print(f"Произошла непредвиденная сетевая ошибка: {e}")
